41e0e05d036bcb26b832c3146e94a5c739f50d17
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-01-22 13:01:07 -0500

    test

--- Server/index.js
+++ Server/index.js
@@ -30,8 +30,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  next();
 });
 
 
@@ -330,7 +328,7 @@ app.get('/api/', function (req, res) {
 });
 
 app.get('/api/test/', function (req, res) {
-  const message = "Status 200: OK" 
+  const message = "test successful" 
   res.json({ message })
 });
 
