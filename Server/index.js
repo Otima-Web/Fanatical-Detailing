cd3b2a172c04940a8c1dbc926090142c638ee8c6
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-01-22 13:06:18 -0500

    update

--- Server/index.js
+++ Server/index.js
@@ -24,15 +24,16 @@ app.use(express.json());
 
 app.use(cors())
 
-// app.use(cors({
-//   origin: '*',
-// }));
+app.use(cors({
+  origin: '*',
+}));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(bodyParser.json());
