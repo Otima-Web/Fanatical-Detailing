7d6fe89ef81a4a41293b46d724a9b6f619de8118
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-01-22 12:04:21 -0500

    update

--- Server/index.js
+++ Server/index.js
@@ -24,6 +24,9 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
   next();
 });
 
